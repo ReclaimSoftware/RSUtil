b2524574eb597d4fa62e2c5c7074e3d75ee81c32
Andrew Schaaf <andrew@andrewschaaf.com>
2014-01-22 21:39:02 -0600

    first public commit

---
+++ RSUtil.c
@@ -0,0 +1,97 @@
+#include <RSUtil.h>
+#include <stdlib.h>
+
+
+void RSFatalError(char *message) {
+    fprintf(stderr, "Fatal error: %s\n", message);
+    fflush(stderr);
+    exit(1);
+}
+
+
+void* RSMallocOrDie(size_t size) {
+    void *data = malloc(size);
+    if(!data) {
+        RSFatalError("malloc");
+    }
+    return data;
+}
+
+
+void* RSCallocOrDie(size_t size) {
+    void *data = calloc(size, 1);
+    if(!data) {
+        RSFatalError("calloc");
+    }
+    return data;
+}
+
+
+void RSFReadOrDie(void *dest, size_t size, FILE *file) {
+    if (fread(dest, size, 1, file) != 1) {
+        RSFatalError("fread");
+    }
+}
+
+
+void RSFWriteOrDie(void *data, size_t size, FILE *file) {
+    if (fwrite(data, size, 1, file) != 1) {
+        RSFatalError("fwrite");
+    }
+}
+
+
+int8_t RSReadInt8(uint8_t *data, uint32_t *offset) {
+    uint32_t i = *offset;
+    *offset = i + 1;
+    return (int8_t)data[i];
+}
+
+
+uint8_t RSReadUInt8(uint8_t *data, uint32_t *offset) {
+    uint32_t i = *offset;
+    *offset = i + 1;
+    return (uint8_t)data[i];
+}
+
+
+int16_t RSReadInt16(uint8_t *data, uint32_t *offset) {
+    uint32_t i = *offset;
+    *offset = i + 2;
+    return ((int16_t *)&(data[i]))[0];
+}
+
+
+uint16_t RSReadUInt16(uint8_t *data, uint32_t *offset) {
+    uint32_t i = *offset;
+    *offset = i + 2;
+    return ((uint16_t *)&(data[i]))[0];
+}
+
+
+int32_t RSReadInt32(uint8_t *data, uint32_t *offset) {
+    uint32_t i = *offset;
+    *offset = i + 4;
+    return ((int32_t *)&(data[i]))[0];
+}
+
+
+uint32_t RSReadUInt32(uint8_t *data, uint32_t *offset) {
+    uint32_t i = *offset;
+    *offset = i + 4;
+    return ((uint32_t *)&(data[i]))[0];
+}
+
+
+int64_t RSReadInt64(uint8_t *data, uint32_t *offset) {
+    uint32_t i = *offset;
+    *offset = i + 8;
+    return ((int64_t *)&(data[i]))[0];
+}
+
+
+uint64_t RSReadUInt64(uint8_t *data, uint32_t *offset) {
+    uint32_t i = *offset;
+    *offset = i + 8;
+    return ((uint64_t *)&(data[i]))[0];
+}
---
+++ RSUtil.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void RSFatalError(char *message);
+void* RSMallocOrDie(size_t size);
+void* RSCallocOrDie(size_t size);
+void RSFReadOrDie(void *dest, size_t size, FILE *file);
+void RSFWriteOrDie(void *data, size_t size, FILE *file);
+
+int8_t RSReadInt8(uint8_t *data, uint32_t *offset);
+uint8_t RSReadUInt8(uint8_t *data, uint32_t *offset);
+int16_t RSReadInt16(uint8_t *data, uint32_t *offset);
+uint16_t RSReadUInt16(uint8_t *data, uint32_t *offset);
+int32_t RSReadInt32(uint8_t *data, uint32_t *offset);
+uint32_t RSReadUInt32(uint8_t *data, uint32_t *offset);
+int64_t RSReadInt64(uint8_t *data, uint32_t *offset);
+uint64_t RSReadUInt64(uint8_t *data, uint32_t *offset);
